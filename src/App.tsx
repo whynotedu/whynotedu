a55d9982aa1d5261c5d33aa86c712b6500df566c
Sooyoung <dannylisa67@gmail.com>
2022-02-19 11:23:01 +0900

    gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
+import { Router } from 'Router';
 
 function App() {
   return (
     <RecoilRoot>
-      
+      <Router />
     </RecoilRoot>
   );
 }
--- src/home/index.tsx
+++ src/home/index.tsx
@@ -2,7 +2,7 @@
 export function Home(){
     return (
         <div>
-
+            ox 페이지 개발 시작!
         </div>
     )
 }
